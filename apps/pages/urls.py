fc2b4dd5af87bf8557ff48a30215b90f4741bcf5
ELEADNRO344 <eleandromartins23@hotmail.com>
2024-10-14 15:21:06 -0300

    adicionado consulta trimed

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -5,7 +5,9 @@ from pages import views
 urlpatterns = [
     path('home/', views.home_view, name='home'),  # Mapeia a URL 'home/' para a view 'home_view'
     # path('unicred/', views.unicred_view, name='unicred'),
-    path('unicred/', views.unicred_view, name='unicred'),
+    path('unicred/', views.unicred_view, name='unicred_view'),
+    path('faturamento/', views.unicred_view, name='unicred_view'),
+
 
 
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+import mysql.connector
+import pandas as pd
 # Create your views here.
 @login_required(login_url='login')  # Redireciona para a página de login se o usuário não estiver autenticado
 def home_view(request):
@@ -10,84 +11,209 @@ def home_view(request):
 
 # apps/views.py
 
-# apps/views.py
-
 from django.shortcuts import render
+import pandas as pd
 import mysql.connector
 
-def unicred_view(request):
-    resultados_remessa = []
-    resultados_retorno = []
-
-    if request.method == 'POST':
-        codigo_doc = request.POST.get('codigo_doc')
-
-        # Conexão com o banco de dados
+def carregar_dados_remessa(codigo_doc=None):
+    try:
         mydb = mysql.connector.connect(
             host='db_sabertrimed.mysql.dbaas.com.br',
             user='db_sabertrimed',
-            password='s@BRtR1m3d',  
+            password='s@BRtR1m3d',
             database='db_sabertrimed',
         )
-
-        cursor = mydb.cursor(dictionary=True)
-
-        # Primeira consulta: Tabela unicredremessa
         consulta_remessa = """
-        SELECT 
-            `Data da Gravação do Arquivo` AS data_gravacao,    
-            `Identificação da Ocorrência` AS identificacao_ocorrencia,
-            `CODIGO DO DOC` AS codigo_doc,    
-            `Data de emissão do Título` AS data_emissao,    
-            `Data de vencimento do Título` AS data_vencimento,    
-            `Valor do Título` AS valor_titulo,    
-            `Nome/Razão Social do Pagador` AS nome_pagador,  
-            `Data Limite P/Concessão de Desconto` AS data_limite_desconto,
-            `Valor do Desconto` AS valor_desconto,
-            `Nº Sequencial do Registro remessa` AS sequencial_registro_remessa, 
-            `Nº Sequencial do Arquivo` AS sequencial_arquivo,
-            `Nº Sequencial do Registro` AS sequencial_registro
-        FROM unicredremessa
-        WHERE `CODIGO DO DOC` = %s
+            SELECT 
+                `Data da Gravação do Arquivo`,
+                `Identificação da Ocorrência`,
+                `CODIGO DO DOC`,
+                `Data de emissão do Título`,
+                `Data de vencimento do Título`,
+                `Valor do Título`,
+                `Nome/Razão Social do Pagador`,
+                `Data Limite P/Concessão de Desconto`,
+                `Valor do Desconto`,
+                `Nº Sequencial do Registro remessa`,
+                `Nº Sequencial do Arquivo`,
+                `Nº Sequencial do Registro`
+            FROM unicredremessa
         """
-        cursor.execute(consulta_remessa, (codigo_doc,))
-        resultados_remessa = cursor.fetchall()
+        if codigo_doc:
+            consulta_remessa += " WHERE `CODIGO DO DOC` = %s"
+            remessaunicred_bd = pd.read_sql(consulta_remessa, con=mydb, params=[codigo_doc])
+            remessaunicred_bd = remessaunicred_bd.sort_values(by='Data da Gravação do Arquivo', ascending=True)
 
-        # Segunda consulta: Tabela retornounicred
+            novos_nomes = {
+                'Data da Gravação do Arquivo': 'Data da Ocorrencia',
+                'Identificação da Ocorrência': 'Ocorrencia',
+                # ... adicione os outros nomes conforme necessário
+            }
+            remessaunicred_bd.rename(columns=novos_nomes, inplace=True)
+            remessaunicred_bd['Data da Ocorrencia'] = pd.to_datetime(remessaunicred_bd['Data da Ocorrencia'], format='%d%m%y', errors='coerce')
+
+            # Formatar a coluna 'Data da Ocorrencia' no novo formato desejado
+            remessaunicred_bd['Data da Ocorrencia'] = remessaunicred_bd['Data da Ocorrencia'].dt.strftime('%d/%m/%Y')                
+        else:
+            remessaunicred_bd = pd.read_sql(consulta_remessa, con=mydb)
+            remessaunicred_bd =  remessaunicred_bd.head(5)            
+
+            remessaunicred_bd = remessaunicred_bd.sort_values(by='Data da Gravação do Arquivo', ascending=True)
+
+            novos_nomes = {
+                'Data da Gravação do Arquivo': 'Data da Ocorrencia',
+                'Identificação da Ocorrência': 'Ocorrencia',
+                # ... adicione os outros nomes conforme necessário
+            }
+            remessaunicred_bd.rename(columns=novos_nomes, inplace=True)
+            remessaunicred_bd['Data da Ocorrencia'] = pd.to_datetime(remessaunicred_bd['Data da Ocorrencia'], format='%d%m%y', errors='coerce')
+
+            # Formatar a coluna 'Data da Ocorrencia' no novo formato desejado
+            remessaunicred_bd['Data da Ocorrencia'] = remessaunicred_bd['Data da Ocorrencia'].dt.strftime('%d/%m/%Y')    
+
+            mydb.close()
+
+        return remessaunicred_bd
+    except mysql.connector.Error as e:
+        print("Erro ao conectar-se ao banco de dados:", e)
+        return pd.DataFrame()
+
+
+def carregar_dados_retorno(codigo_doc=None):
+    try:
+        mydb = mysql.connector.connect(
+            host='db_sabertrimed.mysql.dbaas.com.br',
+            user='db_sabertrimed',
+            password='s@BRtR1m3d',
+            database='db_sabertrimed',
+        )
         consulta_retorno = """
-        SELECT 
-            `DATA DA GERAÇÃO DO ARQUIVO` AS data_geracao,   
-            `TIPO DE INSTRUÇÃO ORIGEM` AS tipo_instrucao,
-            `CÓDIGO DE MOVIMENTO` AS codigo_movimento,
-            `CODIGO DO DOC` AS codigo_doc,
-            `COMPLEMENTO DO MOVIMENTO` AS complemento_movimento,
-            `DATA LIQUIDAÇÃO` AS data_liquidacao,
-            `CANAL DE LIQUIDAÇÃO` AS canal_liquidacao,
-            `VALOR DO TÍTULO` AS valor_titulo,
-            `VALOR ABATIMENTO` AS valor_abatimento,
-            `VALOR PAGO` AS valor_pago,
-            `JUROS DE MORA` AS juros_mora,
-            `VALOR LÍQUIDO` AS valor_liquido,
-            `DATA DE VENCIMENTO` AS data_vencimento,
-            `DATA PROGRAMADA PARA REPASSE` AS data_repasse,
-            `VALOR DA TARIFA` AS valor_tarifa,
-            `DATA DE DEBITO DA TARIFA` AS data_debito_tarifa,
-            `VALOR DESCONTO CONCEDIDO` AS valor_desconto_concedido,
-            `SEQUENCIAL DO REGISTRO` AS sequencial_registro,
-            `NOME DO BENEFICIÁRIO` AS nome_beneficiario,
-            `SEQUENCIAL DO RETORNO` AS sequencial_retorno
-        FROM retornounicred
-        WHERE `CODIGO DO DOC` = %s
+            SELECT 
+                `DATA DA GERAÇÃO DO ARQUIVO`,
+                `TIPO DE INSTRUÇÃO ORIGEM`,
+                `CÓDIGO DE MOVIMENTO`,
+                `CODIGO DO DOC`,
+                `COMPLEMENTO DO MOVIMENTO`,
+                `DATA LIQUIDAÇÃO`,
+                `CANAL DE LIQUIDAÇÃO`,
+                `VALOR DO TÍTULO`,
+                `VALOR ABATIMENTO`,
+                `VALOR PAGO`,
+                `JUROS DE MORA`,
+                `VALOR LÍQUIDO`,
+                `DATA DE VENCIMENTO`,
+                `DATA PROGRAMADA PARA REPASSE`,
+                `VALOR DA TARIFA`,
+                `DATA DE DEBITO DA TARIFA`,
+                `VALOR DESCONTO CONCEDIDO`,
+                `SEQUENCIAL DO REGISTRO`,
+                `NOME DO BENEFICIÁRIO`,
+                `SEQUENCIAL DO RETORNO`
+            FROM retornounicred
         """
-        cursor.execute(consulta_retorno, (codigo_doc,))
-        resultados_retorno = cursor.fetchall()
-        
+        if codigo_doc:
+            consulta_retorno += " WHERE `CODIGO DO DOC` = %s"
+            retornounicred_bd = pd.read_sql(consulta_retorno, con=mydb, params=[codigo_doc])
+            retornounicred_bd['DATA DA GERAÇÃO DO ARQUIVO'] = pd.to_datetime(retornounicred_bd['DATA DA GERAÇÃO DO ARQUIVO'])
 
-        cursor.close()
-        mydb.close()
+            # Ordenar o DataFrame pela coluna 'Data da Ocorrência' de forma crescente
+            #df_retorno = df_retorno.sort_values(by='DATA DA GERAÇÃO DO ARQUIVO')
+            retornounicred_bd = retornounicred_bd.sort_values(by=['DATA DA GERAÇÃO DO ARQUIVO', 'SEQUENCIAL DO REGISTRO'], ascending=[True, True])
+                
+            novos_nomes = {
+                'DATA DA GERAÇÃO DO ARQUIVO': 'Data da Ocorrencia',
+                'CÓDIGO DE MOVIMENTO': 'Ocorrencia',
+                # ... adicione os outros nomes conforme necessário
+            }
+            retornounicred_bd.rename(columns=novos_nomes, inplace=True)
+            #df_retorno.loc[df_retorno['Ocorrencia'] == 'Protesto solicitado', 'Ocorrencia'] = "Enviado a Cartorio"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Protesto solicitado', 'Ocorrencia'] = "Enviado a Cartorio"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Título Descontável (título com desistênc', 'Ocorrencia'] = "Devolvido"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada', 'Ocorrencia'] = "Instrução Confirmada"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Liquidação de Título Descontado', 'Ocorrencia'] = "Liquidação de Título Trocado"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Título Descontado', 'Ocorrencia'] = "Trocado"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Pago(Título protestado pago em cartório)', 'Ocorrencia'] = "Pago em Cartório"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Pedido de ', 'Ocorrencia'] = "Solicitação de Baixa"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Protesto Em cartório', 'Ocorrencia'] = "Em cartório"
+            #df_remessa.loc[df_remessa['TIPO DE INSTRUÇÃO ORIGEM'] == 'Protesto Em cartório', 'TIPO DE INSTRUÇÃO ORIGEM'] = "Em cartório"
 
-    # Passando os resultados das duas tabelas para o template
-    return render(request, 'unicred.html', {
-        'resultados_remessa': resultados_remessa,
-        'resultados_retorno': resultados_retorno,
-    })
+            condicao = (retornounicred_bd['TIPO DE INSTRUÇÃO ORIGEM'] == 'Protestar') & (retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada')
+
+            # Atualizar o valor da coluna "Ocorrencia" para "boleto protestado" onde a condição for verdadeira
+            retornounicred_bd.loc[condicao, 'Ocorrencia'] = 'Boleto Protestado'
+            retornounicred_bd['DATA LIQUIDAÇÃO'] = pd.to_datetime(retornounicred_bd['DATA LIQUIDAÇÃO'], format='%d%m%y', errors='coerce')
+
+
+            condicao = (retornounicred_bd['TIPO DE INSTRUÇÃO ORIGEM'] == ' Pedido de Baixa') & (retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada')
+
+            # Atualizar o valor da coluna "Ocorrencia" para "boleto protestado" onde a condição for verdadeira
+            retornounicred_bd.loc[condicao, 'Ocorrencia'] = 'Boleto Baixado'
+            # Formatar a coluna 'Data da Ocorrencia' no novo formato desejado
+            retornounicred_bd['DATA LIQUIDAÇÃO'] = retornounicred_bd['DATA LIQUIDAÇÃO'].dt.strftime('%d/%m/%Y')
+        else:
+            retornounicred_bd = pd.read_sql(consulta_retorno, con=mydb)
+            retornounicred_bd = retornounicred_bd.head(5)
+
+            retornounicred_bd['DATA DA GERAÇÃO DO ARQUIVO'] = pd.to_datetime(retornounicred_bd['DATA DA GERAÇÃO DO ARQUIVO'])
+
+            # Ordenar o DataFrame pela coluna 'Data da Ocorrência' de forma crescente
+            #df_retorno = df_retorno.sort_values(by='DATA DA GERAÇÃO DO ARQUIVO')
+            retornounicred_bd = retornounicred_bd.sort_values(by=['DATA DA GERAÇÃO DO ARQUIVO', 'SEQUENCIAL DO REGISTRO'], ascending=[True, True])
+                
+            novos_nomes = {
+                'DATA DA GERAÇÃO DO ARQUIVO': 'Data da Ocorrencia',
+                'CÓDIGO DE MOVIMENTO': 'Ocorrencia',
+                # ... adicione os outros nomes conforme necessário
+            }
+            retornounicred_bd.rename(columns=novos_nomes, inplace=True)
+            #df_retorno.loc[df_retorno['Ocorrencia'] == 'Protesto solicitado', 'Ocorrencia'] = "Enviado a Cartorio"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Protesto solicitado', 'Ocorrencia'] = "Enviado a Cartorio"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Título Descontável (título com desistênc', 'Ocorrencia'] = "Devolvido"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada', 'Ocorrencia'] = "Instrução Confirmada"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Liquidação de Título Descontado', 'Ocorrencia'] = "Liquidação de Título Trocado"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Título Descontado', 'Ocorrencia'] = "Trocado"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Pago(Título protestado pago em cartório)', 'Ocorrencia'] = "Pago em Cartório"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Pedido de ', 'Ocorrencia'] = "Solicitação de Baixa"
+            retornounicred_bd.loc[retornounicred_bd['Ocorrencia'] == 'Protesto Em cartório', 'Ocorrencia'] = "Em cartório"
+            #df_remessa.loc[df_remessa['TIPO DE INSTRUÇÃO ORIGEM'] == 'Protesto Em cartório', 'TIPO DE INSTRUÇÃO ORIGEM'] = "Em cartório"
+
+            condicao = (retornounicred_bd['TIPO DE INSTRUÇÃO ORIGEM'] == 'Protestar') & (retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada')
+
+            # Atualizar o valor da coluna "Ocorrencia" para "boleto protestado" onde a condição for verdadeira
+            retornounicred_bd.loc[condicao, 'Ocorrencia'] = 'Boleto Protestado'
+            retornounicred_bd['DATA LIQUIDAÇÃO'] = pd.to_datetime(retornounicred_bd['DATA LIQUIDAÇÃO'], format='%d%m%y', errors='coerce')
+
+
+            condicao = (retornounicred_bd['TIPO DE INSTRUÇÃO ORIGEM'] == ' Pedido de Baixa') & (retornounicred_bd['Ocorrencia'] == 'Instrução Confirmada')
+
+            # Atualizar o valor da coluna "Ocorrencia" para "boleto protestado" onde a condição for verdadeira
+            retornounicred_bd.loc[condicao, 'Ocorrencia'] = 'Boleto Baixado'
+            # Formatar a coluna 'Data da Ocorrencia' no novo formato desejado
+            retornounicred_bd['DATA LIQUIDAÇÃO'] = retornounicred_bd['DATA LIQUIDAÇÃO'].dt.strftime('%d/%m/%Y')            
+
+            mydb.close()
+
+        return retornounicred_bd
+    except mysql.connector.Error as e:
+        print("Erro ao conectar-se ao banco de dados:", e)
+        return pd.DataFrame()
+
+
+def unicred_view(request):
+    codigo_doc = request.GET.get('codigo_doc')
+    remessa_data = carregar_dados_remessa(codigo_doc)
+    retorno_data = carregar_dados_retorno(codigo_doc)
+
+    context = {
+        'remessa': remessa_data.to_dict(orient='records'),
+        'retorno': retorno_data.to_dict(orient='records'),
+        'codigo_doc': codigo_doc,
+    }
+
+    return render(request, 'unicred.html', context)
+
+
+# FATURAMENTO
+
+
+
